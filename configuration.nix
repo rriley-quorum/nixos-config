199dc390ab628bb5653e4903dd9013a3492e7af3
Ryan Riley <ryan.riley@quorumsoftware.com>
2026-08-13 19:48:06 +0000

    chore: update flake and automate nix-store

--- configuration.nix
+++ configuration.nix
@@ -32,9 +32,21 @@
   ];
 
   nix.settings = {
+    auto-optimise-store = true;
     experimental-features = [ "nix-command" "flakes" ];
     download-buffer-size = 524288000; # 500 MiB
   };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  nix.extraOptions = ''
+    min-free = ${toString (100 * 1024 * 1024)}
+    max-free = ${toString (1024 * 1024 * 1024)}
+  '';
 
   security.sudo.wheelNeedsPassword = false;
 
